374e1f87b7e841773e8314558eaa342703304060
dak2 <dawt2h@gmail.com>
2022-08-28 10:35:31 +0900

    jobの引数をActiveRecord objectsからprimitiveなIDに変更
    sidekiqの引数はActiveRecordのobjectsを受け取れないため
    refs. https://github.com/mperham/sidekiq/wiki/The-Basics#client

--- rails/app/services/upload_file_to_kibela_service.rb
+++ rails/app/services/upload_file_to_kibela_service.rb
@@ -1,7 +1,7 @@
 class UploadFileToKibelaService
   def execute
     AttachimentFile.where(kibela_path: nil).find_each do |file|
-      UploadFileToKibelaJob.perform_in(1.second, file)
+      UploadFileToKibelaJob.perform_in(1.second, file.id)
     end
   end
 end
