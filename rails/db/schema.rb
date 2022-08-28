e09e25d4c60101d0adf3ddf3fe00e86e13b0b146
dak2 <dawt2h@gmail.com>
2022-08-28 15:19:19 +0900

    google driveの共有path保存用カラム追加
    refs. https://github.com/starx-inc/docbase_to_kibela/pull/4#issuecomment-1229361926

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_19_120240) do
+ActiveRecord::Schema.define(version: 2022_08_28_061836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_08_19_120240) do
     t.string "kibela_id"
     t.datetime "kibela_updated_at"
     t.string "kibela_path"
+    t.string "drive_path"
   end
 
   create_table "comments", force: :cascade do |t|
