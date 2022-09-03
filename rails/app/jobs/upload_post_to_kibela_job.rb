8ac51de5c6ad22dac371a50cf46e7535f785da84
dak2 <dawt2h@gmail.com>
2022-09-03 14:29:53 +0900

    job名変更: 記事移行用

---
+++ rails/app/jobs/upload_post_to_kibela_job.rb
@@ -0,0 +1,16 @@
+class UploadPostToKibelaJob
+  include Sidekiq::Worker
+  sidekiq_options queue: :default, retry: 3
+
+  def perform
+    # target = AttachimentFile.where(kibela_id: nil).first
+    # target.upload_to_kibela!
+    # UploadKibelaJob.perform_in(1.second)
+  rescue Kibela::ApiError => api_error
+    if api_error.only_rate_limit?
+      UploadPostToKibelaJob.perform_in(1800.second)
+    else
+      pp api_error
+    end
+  end
+end
